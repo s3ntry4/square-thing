89471fb8a5fb03330ae3197db7486d8f0d535a64
s3ntry4 <47262536+s3ntry4@users.noreply.github.com>
2022-05-10 09:50:02 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ const octx = ocanvas.getContext('2d');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const fac = new FastAverageColor();
-const dcanvas = document.getElementById('dcanvas');
-const dctx = dcanvas.getContext('2d');
-
-const acanvas = document.getElementById('acanvas');
-const actx = acanvas.getContext('2d');
 
 const fcanvas = document.getElementById('fcanvas');
 const fctx = fcanvas.getContext('2d');
@@ -31,9 +26,13 @@ function avgcolor(img, x, y, w, h) {
     return color;
 }
 
+var total = 0;
+
 var canv;
 var dcanv;
 var dcanvd;
+
+var current = 40;
 
 var final = false;
 var wid;
@@ -49,12 +48,11 @@ function getarray(img) {
 }
 
 function main() {
+    wid = current//randomIntFromInterval(1, 100);
+    len = current//randomIntFromInterval(1, 75);
 
-    var randx = randomIntFromInterval(0, 100);
-    var randy = randomIntFromInterval(0, 75);
-
-    wid = randomIntFromInterval(1, 100);
-    len = randomIntFromInterval(1, 75);
+    var randx = randomIntFromInterval(0, 100 - wid / 3);
+    var randy = randomIntFromInterval(0, 75 - len / 3);
 
     ctx.clearRect(0, 0, 100, 75);
 
@@ -69,10 +67,7 @@ function main() {
 
     dcanv = octx.getImageData(0, 0, 100, 75);
     dcanvd = ctx.getImageData(0, 0, 100, 75);
-    const diff = dctx.createImageData(100, 75);
-    score = pixelmatch(dcanv.data, dcanvd.data, diff.data, 100, 75, { threshold: 0.1 });
-    //console.log(diff);
-    dctx.putImageData(diff, 0, 0);
+    score = pixelmatch(dcanv.data, dcanvd.data, null, 100, 75, { threshold: 0.1 });
     if (score < bestdiff) {
         bestdiff = score;
         stuff = [randx, randy, wid, len, col];
@@ -80,10 +75,10 @@ function main() {
 }
 
 var gen = document.querySelector('#gen');
-
+var thing = 1;
 function go() {
-    for (let f = 0; f < 1; f++) {
-        for (let c = 0; c < 300; c++) {
+    for (let f = 0; f < document.getElementById("gens").value; f++) {
+        for (let c = 0; c < document.getElementById("tries").value; c++) {
             //bestdiff = 999999999;
             main();
             
@@ -92,7 +87,15 @@ function go() {
         fctx.fillStyle = `rgb(${stuff[4][0]}, ${stuff[4][1]}, ${stuff[4][2]})`;
         fctx.fillRect(stuff[0], stuff[1], stuff[2], stuff[3]);
         final = fctx.getImageData(0, 0, 100, 75);
+
+        console.log(f);
+        total++;
     }
+    current = current * 0.75;
+    console.log(current);
+    console.log(thing);
+    thing++;
+    console.log("total: " + total);
 }
 
 document.querySelector('#btn').addEventListener('click', go)
@@ -107,8 +110,8 @@ document.querySelector('#btn').addEventListener('click', go)
 
 
 
-
-/*const ocanvas = document.getElementById('ocanvas');
+/*
+const ocanvas = document.getElementById('ocanvas');
 const octx = ocanvas.getContext('2d');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -178,7 +181,7 @@ function main() {
     dcanv = octx.getImageData(0, 0, 100, 75);
     dcanvd = ctx.getImageData(0, 0, 100, 75);
     const diff = dctx.createImageData(100, 75);
-    score = pixelmatch(dcanv.data, dcanvd.data, diff.data, 100, 75, { threshold: 0.1 });
+    score = pixelmatch(dcanv.data, dcanvd.data, null, 100, 75, { threshold: 0.1 });
     //console.log(diff);
     dctx.putImageData(diff, 0, 0);
     if (score < bestdiff) {
@@ -190,7 +193,7 @@ function main() {
 var gen = document.querySelector('#gen');
 
 function go() {
-    for (let f = 0; f < 50; f++) {
+    for (let f = 0; f < 100; f++) {
         for (let c = 0; c < 30; c++) {
             //bestdiff = 999999999;
             main();
@@ -200,6 +203,7 @@ function go() {
         fctx.fillStyle = `rgb(${stuff[3][0]}, ${stuff[3][1]}, ${stuff[3][2]})`;
         fctx.fillRect(stuff[0], stuff[1], stuff[2], stuff[2]);
         final = fctx.getImageData(0, 0, 100, 75);
+        console.log(f);
     }
 }
 
